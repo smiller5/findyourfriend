a9bce229413b30ed1e3ce64792406df511f6586c
smiller5 <smiller5@ramapo.edu>
2016-06-19 07:45:19 -0400

    new

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,97 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+   {
+      "name":"Stephen Miller",
+      "photo":"https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/12279040_10208300250207230_283434833424256193_n.jpg?oh=3adfc277f0739f1e103290b538382439&oe=57CEDAB8",
+      "scores":[
+         "5",
+         "1",
+         "3",
+         "2",
+         "4",
+         "3",
+         "3",
+         "3"
+         
+      ]
+   },
+   {
+      "name":"Izik Glutkin",
+      "photo":"https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/12647058_10208208285352251_7223653435225475911_n.jpg?oh=8107d5436270f2cdb5e32b43042b7c30&oe=57C23576",
+      "scores":[
+         "4",
+         "2",
+         "3",
+         "1",
+         "2",
+         "1",
+         "2",
+         "1"
+      ]
+   },
+   {
+      "name":"Bryan Halpern",
+      "photo":"https://scontent.fewr1-2.fna.fbcdn.net/t31.0-8/12309678_10153338134396376_5971156730851814958_o.jpg",
+      "scores":[
+         "5",
+         "2",
+         "3",
+         "1",
+         "3",
+         "4",
+         "5",
+         "5"
+      ]
+   },
+   {
+      "name":"Andrew Grossman",
+      "photo":"https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/13118896_10207926130784696_6430948868725086981_n.jpg?oh=dae7dded3fb0734efb382a655900c820&oe=57E24E6A",
+      "scores":[
+         "3",
+         "3",
+         "3",
+         "2",
+         "1",
+         "1",
+         "1",
+         "1"
+      ]
+   },
+	{
+	   "name":"Jeff Himmelson",
+	   "photo":"https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/10264657_10154449177668662_3843641897327101430_n.jpg?oh=d0e9a3dee95ca98a83cee87d13e5e32c&oe=57D83643",
+	   "scores":[
+	      "4",
+	      "3",
+	      "3",
+         "1",
+         "2",
+         "5",
+         "5",
+         "3"
+	   ]
+	},
+	{
+	   "name":"Alex Crane",
+	   "photo":"https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/13322131_10153640180994071_8122124364879299118_n.jpg?oh=66104c07a8d7d2cabe797c0467a28c8f&oe=57E5B0A6",
+	   "scores":[
+	      "4",
+	      "4",
+	      "3",
+         "4",
+         "4",
+         "4",
+         "4",
+         "4"
+	   ]
+	}
+
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
